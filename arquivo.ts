f286c61c357bf5ce8dac628d9d074abf39339c04
Aluno <erosm5287@gmail.com>
2026-03-26 22:01:10 -0300

    guarana 2

---
+++ arquivo.ts
@@ -0,0 +1,5 @@
+let num1: number = 15;
+let num2: number = 15;
+function somar (a:number, b:number):number{
+return a + b;
+}
---
+++ calculadora.ts
@@ -0,0 +1,28 @@
+let num1: number = 5;
+let num2: number = 5;
+
+class calculador {
+     soma(a: number,b: number):number{
+        return a + b;
+        
+    }
+     subtraçao (a: number,b: number):number{
+        return a - b;
+    }
+    dividir (a: number,b: number):number{
+        return a * b;
+    }
+     multiplicaçao (a: number,b: number):number{
+        return a / b;
+    }
+}
+
+/*console.log("digite:\n + para somar \n - subtrair \n* para dividir\n/ para multiplicacao ");
+
+    
+
+    console.log( soma(num1, num2) );
+    console.log( subtraçao(num1, num2) );
+    console.log( dividir(num1, num2) );
+    console.log( multiplicaçao(num1, num2) );
+/*
